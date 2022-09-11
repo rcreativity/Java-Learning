e3d022988d6c418694c5b3e08b1022fea805dadd
Ravikumar R <ravikumar.r22@gmail.com>
2022-09-12 05:24:27 +0530

    Rewalking Basics

--- src/com/learn/Main.java
+++ src/com/learn/Main.java
@@ -1,8 +1,9 @@
-package com.company;
+package com.learn;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.printf("Learn it again");
     }
 }
